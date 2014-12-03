2604d1309c35dbe3ca3a928258c4b17474d4c03d
Daniel Neighman <has.sox@gmail.com>
2014-12-02 22:08:57 -0800

    Get the example working

--- examples/node.js
+++ examples/node.js
@@ -8,9 +8,10 @@ registry.register(require('../proto-bundle'));
 Fender.prepareClientServiceHandlers(scope.services());
 
 AccountService = scope.lookup('services.AccountService');
+token = "<insert your token here>";
 
-new AccountService().Info()
-.then(function(account){ 
+new AccountService({ headers: { Authorization: "Bearer " + token }}).Info()
+.then(function(account){
   console.log("The account is:", require('util').inspect(account.asJSON(), { depth: null} ));
 }).catch(function(e) {
   console.error(e);
