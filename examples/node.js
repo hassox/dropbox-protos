4d6986800ddabc326fa5957a245a5f0b96d2c990
Daniel Neighman <has.sox@gmail.com>
2014-12-02 22:43:51 -0800

    Add a custom stack example

--- examples/node.js
+++ examples/node.js
@@ -15,25 +15,27 @@ AccountService = scope.lookup('services.AccountService');
 
 token = require('./creds').token;
 
-// dropboxStack = mach.stack();
-// dropboxStack.use(function(app) {
-//   return function(conn) {
-//     console.log("THE TOKEN IS:", token);
-//     conn.request.auth = "Bearer " + token;
-//     conn.call(app);
-//   };
-// });
+dropboxAuth = function(app) {
+  return function(conn) {
+    // This line works
+    conn.request.headers.Authorization = "Bearer " + token;
+
+    // This line does not
+    // conn.request.auth = 'Bearer ' + token;
+    return conn.call(app);
+  };
+};
 
 // Get the info for an account
 new AccountService({ headers: { Authorization: "Bearer " + token }}).Info()
 .then(function(account){
   console.log("The account is:", require('util').inspect(account.asJSON(), { depth: null} ));
 
-  // // Using a custom stack
-  // new AccountService({stack: dropboxStack}).Info()
-  // .then(function(secondAccount) {
-  //   console.log("USING A CUSTOM STACK:", require('util').inspect(secondAccount.asJSON(), { depth: null }));
-  // });
+  // Using a custom stack
+  new AccountService({stack: dropboxAuth}).Info()
+  .then(function(secondAccount) {
+    console.log("\n\nUSING A CUSTOM STACK:", require('util').inspect(secondAccount.asJSON(), { depth: null }));
+  });
 
 
 }).catch(function(e) {
